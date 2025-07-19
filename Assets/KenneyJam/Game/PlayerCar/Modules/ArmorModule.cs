f92af5f4ad62bc3f80026d560597387107502785
alex L-B <alexandre.lemarbre@gmail.com>
2025-07-19 12:28:46 -0400

    added armor module code

---
+++ Assets/KenneyJam/Game/PlayerCar/Modules/ArmorModule.cs
@@ -0,0 +1,14 @@
+﻿namespace KenneyJam.Game.PlayerCar.Modules
+{
+    public class ArmorModule : CarModule
+    {
+        public override Type GetModuleType()
+        {
+            return Type.Armor;
+        }
+
+        public override void Activate()
+        {
+        }
+    }
+}
