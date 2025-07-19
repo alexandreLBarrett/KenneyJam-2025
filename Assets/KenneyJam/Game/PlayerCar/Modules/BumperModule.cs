c6b752e71e33fec84b8cfd3faf3b83b968652672
alex L-B <alexandre.lemarbre@gmail.com>
2025-07-19 12:28:30 -0400

    Added all module type data

---
+++ Assets/KenneyJam/Game/PlayerCar/Modules/BumperModule.cs
@@ -0,0 +1,14 @@
+﻿namespace KenneyJam.Game.PlayerCar.Modules
+{
+    public class BumperModule : CarModule
+    {
+        public override Type GetModuleType()
+        {
+            return Type.Bumper;
+        }
+
+        public override void Activate()
+        {
+        }
+    }
+}
