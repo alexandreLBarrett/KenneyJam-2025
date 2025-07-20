0ca3dff1e3128112dec331ddc37b913213783c0f
Akahara <albincalais@gmail.com>
2025-07-20 10:11:50 -0400

    feat: controllable controller

--- Assets/KenneyJam/Game/PlayerController.cs
+++ Assets/KenneyJam/Game/PlayerController.cs
@@ -44,11 +44,30 @@ public class PlayerCarController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        if (gamepad != null)
-        {
-            moveValue.x += gamepad.JoystickX;
-            moveValue.y += gamepad.JoystickY;
-        }
+
+        Vector3 flatForward = Camera.main.transform.forward;
+        flatForward.y = 0;
+        flatForward.Normalize();
+        Vector3 flatRight = Camera.main.transform.right;
+        flatRight.y = 0;
+        flatRight.Normalize();
+
+        Vector3 worldDir = flatForward * gamepad.JoystickY + flatRight * gamepad.JoystickX;
+        
+        float dx = Vector3.Dot(carController.transform.right, worldDir);
+        float dy = Vector3.Dot(carController.transform.forward, worldDir);
+        float steering = Mathf.Clamp(dx / 1f, -1, +1) * .8f;
+        float engine = dy;
+        engine = Mathf.Min(Mathf.Sqrt(1 - steering * steering), Mathf.Abs(engine)) * Mathf.Sign(engine);
+        if (engine < 0) steering *= -1;
+        moveValue.y += engine;
+        moveValue.x += steering;
+
+        //if (gamepad != null)
+        //{
+        //    moveValue.x += gamepad.JoystickX;
+        //    moveValue.y += gamepad.JoystickY;
+        //}
         carController.UpdateMovement(moveValue.y, moveValue.x);
     }
 
